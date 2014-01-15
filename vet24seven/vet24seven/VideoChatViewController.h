f330c7e6e0ddf6c1764742ec4dc6af7ab7d364d0
Edgar A. Nunez <edgar.a.nunez@gmail.com>
2014-01-14 20:25:15 -0800

    Next commit - working rev of ShowKit integration.

---
+++ vet24seven/vet24seven/VideoChatViewController.h
@@ -0,0 +1,17 @@
+//
+//  VideoChatViewController.h
+//  vet24seven-ipad
+//
+//  Created by Edgar Nunez on 1/13/14.
+//  Copyright (c) 2014 Edgar Nunez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <ShowKit/ShowKit.h>
+
+@interface VideoChatViewController : UIViewController
+
+@property (readwrite, nonatomic) UIView *mainVideoUIView;
+@property (readwrite, nonatomic) UIView *prevVideoUIView;
+
+@end
--- vet24seven/vet24seven/ViewController.m
+++ vet24seven/vet24seven/ViewController.m
@@ -33,6 +33,13 @@
     NSURL *_url = [NSURL URLWithString: VET24SEVEN_URL_IPHONE];
     [appWebView loadRequest: [NSURLRequest requestWithURL: _url]];
     
+    [[NSNotificationCenter defaultCenter]
+     addObserver:self
+     selector:@selector(connectionStateChanged:)
+     name:SHKConnectionStatusChangedNotification
+     object:nil];
+
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -41,10 +48,53 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void) webViewDidFinishLoad:(UIWebView *)webView {
+- (BOOL) webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
     
+    NSLog(@"request url: %@", request.URL.absoluteString);
+    
+    if ([request.URL.absoluteString isEqualToString: @"http://demo.vet24seven.com/choose.php?user_id=3"]) {
+        [ShowKit login: @"238.edgar.a.nunezgmail.com" password: @"123456"];
+    }
+    
+    NSString *confirmURL = @"http://demo.vet24seven.com/confirmation.php?referenceNumber=";
+    NSRange range = [request.URL.absoluteString rangeOfString: confirmURL];
+    
+    if (range.location != NSNotFound) {
+        [ShowKit initiateCallWithSubscriber: @"238.calbertlai"];
+    }
+    
+    return YES;
+}
 
+#pragma mark -
+#pragma mark ShowKit callbacks
+
+- (void) connectionStateChanged: (NSNotification*) notification
+{
+    SHKNotification* showNotice;
+    NSString* value;
     
+    showNotice = (SHKNotification*) [notification object];
+    value = (NSString*) showNotice.Value;
+    
+    if ([value isEqualToString: SHKConnectionStatusCallTerminated]){
+        //call is terminated
+    } else if ([value isEqualToString: SHKConnectionStatusInCall]) {
+        //call just got changed to in call,
+        
+    } else if ([value isEqualToString: SHKConnectionStatusLoggedIn]) {
+        NSLog(@"238.edgar.a.nunezgmail.com is logged in.");
+        
+    } else if ([value isEqualToString: SHKConnectionStatusNotConnected]) {
+        //user is no longer connected
+    } else if ([value isEqualToString: SHKConnectionStatusLoginFailed]) {
+        //login has failed
+        NSLog(@"user login failed.");
+    } else if ([value isEqualToString: SHKConnectionStatusCallIncoming]) {
+        //user has a call incoming, accept or reject the call
+        UIAlertView *callAlert = [[UIAlertView alloc] initWithTitle: @"Incoming V-Consult" message: @"You have an incoming V-Consult. Do you want to accept this consultation & begin a video session?" delegate: self cancelButtonTitle: @"No" otherButtonTitles: @"Yes", nil];
+        [callAlert show];
+    }
 }
 
 @end
